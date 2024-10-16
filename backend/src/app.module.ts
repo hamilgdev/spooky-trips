cebeecc64b83a0d0ad07211e8b037d0707f6bf23
hamilgdev <hamilgdev@gmail.com>
2024-10-15 22:13:19 -0500

    feat: Add SpookyModule and FilesModule to app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { HealthCheckModule } from '@/src/modules/health-check/health-check.module';
+import { SpookyModule } from '@/src/modules/spooky/spooky.module';
+import { FilesModule } from '@/src/modules/files/files.module';
 
 @Module({
-  imports: [HealthCheckModule],
+  imports: [HealthCheckModule, SpookyModule, FilesModule],
   controllers: [],
   providers: [],
 })

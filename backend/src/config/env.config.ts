3eea4a2abd20b77603ef487d2cc8faa509b2b051
hamilgdev <hamilgdev@gmail.com>
2024-10-15 22:09:35 -0500

    feat: Add Cloudinary configuration to .env.template and package.json

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -6,6 +6,9 @@ interface EnvVars {
   NEST_PORT: number;
   NODE_ENV: string;
   CORS_ORIGIN: string;
+  CLOUDINARY_CLOUD_NAME: string;
+  CLOUDINARY_API_KEY: string;
+  CLOUDINARY_API_SECRET: string;
 }
 
 const envVarsSchema = joi
@@ -13,6 +16,9 @@ const envVarsSchema = joi
     NEST_PORT: joi.number().required(),
     NODE_ENV: joi.string().required(),
     CORS_ORIGIN: joi.string().required(),
+    CLOUDINARY_CLOUD_NAME: joi.string().required(),
+    CLOUDINARY_API_KEY: joi.string().required(),
+    CLOUDINARY_API_SECRET: joi.string().required(),
   })
   .unknown(true);
 
@@ -26,4 +32,7 @@ export const envs = {
   NEST_PORT: envVars.NEST_PORT,
   NODE_ENV: envVars.NODE_ENV,
   CORS_ORIGIN: envVars.CORS_ORIGIN,
+  CLOUDINARY_CLOUD_NAME: envVars.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: envVars.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: envVars.CLOUDINARY_API_SECRET,
 };

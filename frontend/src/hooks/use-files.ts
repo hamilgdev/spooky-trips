5322f6462c37902a4718450d7c8795eedaf6b00a
hamilgdev <hamilgdev@gmail.com>
2024-10-18 18:53:55 -0500

    refactor: Handle error in useUploadFile hook and update air story flag

--- frontend/src/hooks/use-files.ts
+++ frontend/src/hooks/use-files.ts
@@ -38,6 +38,7 @@ export function useUploadFile() {
         }
       } catch (error) {
         errorHandler(error);
+        setAirStory(false);
       } finally {
         setIsLoading(false);
       }

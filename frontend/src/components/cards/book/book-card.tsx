14b448f2e19d54b82e6bd90037eb25c81f21f989
hamilgdev <hamilgdev@gmail.com>
2024-10-19 17:14:38 -0500

    refactor: Update BookCard component to wrap the book image in a Link component

--- frontend/src/components/cards/book/book-card.tsx
+++ frontend/src/components/cards/book/book-card.tsx
@@ -1,18 +1,22 @@
+import Link from 'next/link';
+
 import './styles.css';
 
 /* eslint-disable @next/next/no-img-element */
 export const BookCard = () => {
   return (
     <ul className='list-inline'>
-      <li className='book shadow-2xl'>
-        <img
-          className='pointer-events-none'
-          src='/assets/images/book-covers/book_cover_transformed_2.webp'
-          alt='cover book'
-          width={1080}
-          height={1920}
-        />
-      </li>
+      <Link href='/spooky-studio/new-story'>
+        <li className='book shadow-2xl'>
+          <img
+            className='pointer-events-none'
+            src='/assets/images/book-covers/book_cover_transformed_2.webp'
+            alt='cover book'
+            width={1080}
+            height={1920}
+          />
+        </li>
+      </Link>
     </ul>
   );
 };
